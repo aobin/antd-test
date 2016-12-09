ed63e1367cde6553d603ac45902e4d7c94f2b341
aobin <ao.bin@hp.com>
2016-12-09 16:47:16 +0800

    添加国际化的功能

--- js/simple.js
+++ js/simple.js
@@ -3,6 +3,8 @@ import React from "react";
 import ReactDom from "react-dom";
 import {Provider} from 'react-redux';
 import { configureStore} from './reducers/ReducerFactory';
+import {LocaleProvider} from "antd";
+import enUS from 'antd/lib/locale-provider/en_US';
 
 
 const store =  configureStore();
@@ -10,7 +12,9 @@ const store =  configureStore();
 
 ReactDom.render(
     <Provider store={store}>
-        <AntSortTable/>
+        <LocaleProvider locale={enUS}>
+            <AntSortTable />
+        </LocaleProvider>
     </Provider>,
     document.getElementById('simple')
 );
