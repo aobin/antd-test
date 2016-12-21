8b8fa6f8ec247ee40d1948050e692fc35babbe02
aobin <ao.bin@hp.com>
2016-12-21 16:51:48 +0800

    1.添加了字体的样式以及进行打包的配置 2.添加了file-loader

--- js/components/Items.js
+++ js/components/Items.js
@@ -4,6 +4,8 @@ import {bindActionCreators} from "redux";
 import {connect} from "react-redux";
 
 require("../../styles/items.css");
+require("../../styles/style.scss");
+
 /*
 require("url-loader?mimetype=image/png!../../images/img_1.png");
 */
@@ -30,7 +32,7 @@ export class Items extends Component
                     <section id={index} className="rss-section show-inline">
                         {/*<img id={index} className="rss-section-img" src="images/img_1.png"/>*/}
                         <div id={index} className="rss-section-content">
-                            <h3>{item.title}</h3>
+                            <h3 className="countrySelector">{item.title}</h3>
                             <div  className="rss-picture"></div>
                             <span><a href="#">{antPaginationReducer.currentPage}</a> Likes</span>
                             <span><a href="#"> {antPaginationReducer.currentPage}</a> Comment</span>
--- webpack.config.js
+++ webpack.config.js
@@ -37,19 +37,20 @@ module.exports = {
                 }
             },
             {
-                test: /\.scss$/,
+                test: /\.(scss|css)$/,
                 loaders: ['style', 'css?sourceMap', 'sass?sourceMap'],
                 include: STYLE_PATH
             },
             {
-                test: /\.css$/,
-                loaders: ['style', 'css?sourceMap', 'sass?sourceMap'],
-                include: STYLE_PATH
-            },
-            {
-                test: /\.(png)|(jpg)$/,
+                test: /\.(png|jpg)$/,
                 loader: 'url?limit=40000'
             },
+            { test: /\.svg$/, loader: 'url?limit=65000&mimetype=image/svg+xml&name=[name].[ext]' },
+            { test: /\.woff$/, loader: 'url?limit=65000&mimetype=application/font-woff&name=[name].[ext]' },
+            { test: /\.woff2$/, loader: 'url?limit=65000&mimetype=application/font-woff2&name=[name].[ext]' },
+            { test: /\.[ot]tf$/, loader: 'url?limit=65000&mimetype=application/octet-stream&name=[name].[ext]' },
+            { test: /\.eot$/, loader: 'url?limit=65000&mimetype=application/vnd.ms-fontobject&name=[name].[ext]' },
+
             {
                 test: /\.less$/,
                 loader: 'style-loader!css-loader!autoprefixer-loader!less-loader'
