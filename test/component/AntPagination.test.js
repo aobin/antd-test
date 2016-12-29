5b3db87e0b6e1eb5594187481eac2e6fc3bb85c9
aobin <ao.bin@hp.com>
2016-12-29 09:51:53 +0800

    基本的事件模拟完成。

--- test/component/AntPagination.test.js
+++ test/component/AntPagination.test.js
@@ -7,25 +7,35 @@ import {shallow,mount} from "enzyme";
 
 describe("AntPagination Unit Test", ()=>
          {
-             it("AntPagination: div created", ()=>
-             {
-                 /*let props;*/
-                 let antPaginationReducer;
-                 let antPagination;
-                 let loadFeedDocumentFromServer=()=>{};
-                 antPaginationReducer = {url:"123",currentPage:10,pageSize:1,total:100};
-                /* props={
-                     antPaginationReducer:antPaginationReducer,
-                     loadFeedDocumentFromServer:()=>{}
-                 };*/
+             let antPaginationReducer;
+             let antPagination;
+             let loadFeedDocumentFromServer;
+             let setAgeSort;
 
-                 antPagination = shallow(<AntPagination antPaginationReducer={antPaginationReducer}  loadFeedDocumentFromServer={loadFeedDocumentFromServer}/>);
-                 expect(antPagination.find("div").length).toEqual(1);
-                 console.log("antPagination.length:",antPagination.length);
-                 console.log("antPagination",antPagination);
+             beforeEach(() =>
+            {
+                 loadFeedDocumentFromServer=jasmine.createSpy('loadFeedDocumentFromServer');
+                 setAgeSort = jasmine.createSpy("setAgeSort");
+                 antPaginationReducer = {url:"123",currentPage:10,pageSize:1,total:100};
+                 antPagination = shallow(<AntPagination antPaginationReducer={antPaginationReducer}  loadFeedDocumentFromServer={loadFeedDocumentFromServer} setAgeSort={setAgeSort} />);
+                 console.log("==="+JSON.stringify(antPaginationReducer)+"====");
              });
 
+             it("AntPagination: div created and loadFeedDocumentFromServer is called when created", ()=>
+             {
+                 expect(antPagination.find("div").length).toEqual(1);
+                 expect(loadFeedDocumentFromServer).toHaveBeenCalled();
 
+             });
+
+             it("AntPagination: simulate onChange event", ()=>
+             {
+                 let pagination=antPagination.find("Pagination")
+                 expect(pagination.length).toEqual(1);
+                 pagination.simulate('change');
+                 expect(loadFeedDocumentFromServer).toHaveBeenCalled();
+                 expect(setAgeSort).toHaveBeenCalled();
+             });
 
          }
 );
